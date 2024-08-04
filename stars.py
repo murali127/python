a2f719e0f99e0c9fc2610b43543ce7a6078edb94
Murali_paila <114466262+murali127@users.noreply.github.com>
2024-08-04 12:36:11 +0000

    ASDFGHJ

--- stars.py
+++ stars.py
@@ -8,3 +8,8 @@ n = int(input("Enter the number: "))
 for i in range(1, n+1):
     print(" " * (n - i) + "*" * i)
 
+n = int(input("Enter the number: "))
+for i in range(1, n+1):
+    for j in range(1, i+1):
+        print(j, end=" ")
+    print()
