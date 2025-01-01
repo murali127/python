6f0e7412bf61d616fcf572159ee669db2ae8cd7a
Paila Murali <114466262+murali127@users.noreply.github.com>
2025-01-01 21:56:33 +0530

    Update stars.py

--- stars.py
+++ stars.py
@@ -13,3 +13,13 @@ for i in range(1, n+1):
     for j in range(1, i+1):
         print(j, end=" ")
     print()
+n = int(input("Enter the number: "))
+for i in range(1,n+1,1):
+    print(" " * (n - i) + "*" * (2 * i - 1))
+for i in range(n,0,-1):
+    print(" " * (n-i)+"*" * (2*i-1))
+print()
+print("reverse star")
+for i in range(n,0,-1):
+        print(" "*(n-i)+"*"*(2*i-1))
+    
