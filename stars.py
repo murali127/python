f478a2226771e40fc6aaa34eda0ebfde553e996f
Paila Murali <114466262+murali127@users.noreply.github.com>
2025-01-02 20:10:57 +0530

    Update stars.py

--- stars.py
+++ stars.py
@@ -22,4 +22,16 @@ print()
 print("reverse star")
 for i in range(n,0,-1):
         print(" "*(n-i)+"*"*(2*i-1))
-    
+print("binary stars")
+for i in range(1,n+1,1):
+        for j in range(i):
+            print((i+j)%2,end=" ")
+        print( )
+print("double triangle")
+for i in range(1,n+1,1):
+        for j in range(1,i+1):
+            print(j,end=" ")
+        print(" "*(n-i),end="")
+        for j in range(i,0,-1):
+            print(j,end=" ")
+        print( )
