399c91f33d5e483714512c14790f56cd2581877d
Paila Murali <114466262+murali127@users.noreply.github.com>
2025-07-23 14:04:02 +0000

    dynarr

--- dyn_array.py
+++ dyn_array.py
@@ -16,6 +16,50 @@ class ml:
         #append
         self.a[self.n]=item
         self.n=self.n+1
+#4. Print
+    def __str__(self):
+        res=''
+        for i in range(self.n):
+            res=res+str(self.a[i])+ ','
+        return '['+ res[:-1] +']'
+#5. Indexing 
+    def __getitem__(self,index):
+        if 0<=index<self.n:
+            return self.a[index]
+        else:
+            return 'Indexerror - Index out of range'
+#6. Pop 
+    def pop(self):
+        if self.n==0:
+            return 'Empty List'
+        print(self.a[self.n-1])
+        self.n=self.n-1
+#7. Clear
+    def clear(self):
+        self.n=0
+        self.size=1
+#8. Find
+    def find(self,item):
+        for i in range (self.n):
+            if self.a[i]==item:
+                return i 
+        return 'not in list'
+
+#9. Insert 
+    def insert(self,pos,item):
+        if self.n==self.size:
+            self.__resize(self.size*2)
+        for i in range(self.n,pos-1):
+            self.a[i]=self.a[i-1]
+        self.a[pos]=item 
+        self.n=self.n+1
+#10. Delete 
+    def __delitem__(self,pos):
+        if 0<=pos<self.n:
+            for i in range(pos,self.n-1):
+                self.a[i]=self.a[i+1]
+            self.n=self.n-1
+     
     def __resize(self,new_capacity):
         #create array with new capacity
         b=self.__make_array(new_capacity)
@@ -35,6 +79,19 @@ L.append(3.4)
 L.append(True)
 L.append(100)
 L.append(1)
+L.append(45)
 print(len(L))
 print(L.size)
+print(L)
+print(L[10])
+L.pop()
+print(L)
+L.insert(5,45)
+print(L)
+print(L.find(100))
+del L[6]
+print(L)
+L.clear()
+print(L)
+L.insert(0,0)
 print(L)
