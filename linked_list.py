cf4fe3bdebf129b59f9a5aa9a2eff755166d7f88
Paila Murali <114466262+murali127@users.noreply.github.com>
2025-07-28 16:28:15 +0000

    del

--- linked_list.py
+++ linked_list.py
@@ -20,12 +20,30 @@ class ll:
 #6. Insert from tail
     def insert_tail(self,value):
         new_node=node(value)
+        if self.head==None:
+            self.head=new_node
+            self.n=self.n+1
+            return
         curr=self.head
         while curr.next!=None:
             curr=curr.next
         curr.next=new_node
+        self.n=self.n+1
+
 #7. Insert at middle
-    
+    def insert_after(self,after,value):
+        new_node=node(value)
+        curr=self.head 
+        while curr!=None:
+            if curr.data==after:
+                break
+            curr=curr.next 
+        if curr!=None:
+            new_node.next=curr.next 
+            curr.next=new_node
+            self.n=self.n+1
+        else:
+            print('Item not found')
 #5. Traverse
     def traverse(self):
         curr=self.head
@@ -33,6 +51,10 @@ class ll:
             print(curr.data,end="->")
             curr=curr.next 
         print("null") 
+#6. Empty 
+    def empty(self):
+        self.head=None 
+        self.n=0
 
 L=ll()
 while True:
@@ -42,7 +64,9 @@ while True:
     print("1. Length")
     print("2. Insert Head")
     print("3. Insert From Tail")
+    print("4. Insert at Middle")
     print("5. Traverse the list")
+    print("6. Empty")
     print("10. Exit")
     choice=int(input("Enter your choice : "))
     if choice==1:
@@ -53,8 +77,14 @@ while True:
     elif choice==3:
         a=int(input("enter the vlaue to insert : "))
         L.insert_tail(a)
+    elif choice==4:
+        aft=int(input("enter thre after number :"))
+        a=int(input("enter the vlaue to insert : "))
+        L.insert_after(aft,a)
     elif choice==5:
         L.traverse()
+    elif choice==6:
+        L.empty()
     elif choice==10:
         print("ok byee")
         break
